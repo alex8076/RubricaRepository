5c2532f3138c8b0f8a971caa36b944887fbe578c
Giovanni-Caldarelli <giovannicaldarelli4@gmail.com>
2024-12-07 16:53:28 +0100

    Creazione classe Rubrica

---
+++ src/main/java/com/mycompany/rubricaproject/Rubrica.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.rubricaproject;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author giovannicaldarelli
+ */
+public class Rubrica {
+     private Set<Contatto> contatti;
+     
+     
+     
+     public Rubrica() {
+         this.contatti=new HashSet<>();
+     }
+     
+     
+     
+     public Set<Contatto> getContatti() {
+        
+    }
+     
+     
+    
+    public void aggiungiContatto(Contatto c) {
+        
+    }
+    
+    
+    public void rimuoviContatto(Contatto c) {
+        
+    }
+    
+}
