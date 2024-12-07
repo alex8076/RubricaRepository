079a97d508bdca821d901d83408734c75eb778bb
GaetanoDonnarumma <g.donnarumma58@studenti.unisa.it>
2024-12-07 18:32:12 +0100

    Creazione e Documentazione dell'interfaccia FileHandler

--- src/main/java/com/mycompany/rubricaproject/Contatto.java
+++ src/main/java/com/mycompany/rubricaproject/Contatto.java
@@ -1,12 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.rubricaproject;
-
-import java.util.Set;
-
 /**
  *
  * @file Contatto.java
@@ -21,6 +12,9 @@ import java.util.Set;
  * @date 2024-12-07
  */
 
+package com.mycompany.rubricaproject;
+
+import java.util.Set;
 
 public class Contatto implements Comparable<Contatto> {
     
