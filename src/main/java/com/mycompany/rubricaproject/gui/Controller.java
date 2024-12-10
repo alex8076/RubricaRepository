07ffe30f1d1a81027c5457262998f88aa239fce3
GaetanoDonnarumma <g.donnarumma58@studenti.unisa.it>
2024-12-10 13:17:12 +0100

    Update Controller.java

--- src/main/java/com/mycompany/rubricaproject/gui/Controller.java
+++ src/main/java/com/mycompany/rubricaproject/gui/Controller.java
@@ -364,6 +364,7 @@ public class Controller implements Initializable {
      */
     @FXML
     private void handleCancel(ActionEvent event) {
+        // ripulisco i campi input
         ripulisciCampi();
     }
     
